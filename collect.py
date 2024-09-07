2dfa8bec69587c8833cd1cc0db73c05cf24d0cf3
RezgoolGit <84568322+RezgoolGit@users.noreply.github.com>
2024-09-07 22:56:32 +0330

    Update collect.py

--- collect.py
+++ collect.py
@@ -25,10 +25,7 @@ def collect():
     }
 
     providers = json.load(open('providers.json'))
-    try:
-        existing_ips = json.load(open('list.json'))
-    except FileNotFoundError: 
-        existing_ips = {"ipv4": [], "ipv6": []} 
+    existing_ips = json.load(open('list.json'))
     last_update = 0
 
     resolver = dns.resolver.Resolver()
@@ -41,22 +38,17 @@ def collect():
             ipv4_result = resolver.resolve(provider, "A")
             for ipv4 in ipv4_result:
                 ip = ipv4.to_text()
-                if ip not in [el["ip"] for el in result["ipv4"]]: 
-                    prev = next((el for el in existing_ips["ipv4"] if el["ip"] == ip), None)
-                    created_at = prev["created_at"] if prev else int(time.time())
-                    last_update = created_at if created_at > last_update else last_update
-                    result["ipv4"].append({
-                        "ip": ip,
-                        "operator": providers[provider],
-                        "provider": '.'.join(provider.split('.')[1:]),
-                        "created_at": created_at
-                    })
+                prev = next((el for el in existing_ips["ipv4"] if el["ip"] == ip), None)
+                created_at = prev["created_at"] if prev else int(time.time())
+                last_update = created_at if created_at > last_update else last_update
+                result["ipv4"].append({
+                    "ip": ip,
+                    "operator": providers[provider],
+                    "provider": '.'.join(provider.split('.')[1:]),
+                    "created_at": created_at
+                })
         except:
             pass
-
-    # Filter out IPs that are present in the existing_ips file
-    new_ipv4 = [el for el in result["ipv4"] if el["ip"] not in [x["ip"] for x in existing_ips["ipv4"]]]
-    result["ipv4"] = new_ipv4
 
     result["last_update"] = datetime.fromtimestamp(last_update).__str__()
     result["last_timestamp"] = last_update
@@ -64,6 +56,16 @@ def collect():
     result["ipv4"].sort(key=lambda el: el["created_at"], reverse=True)
     result["ipv4"].sort(key=lambda el: el["operator"])
     return result
+
+class IP:
+    def __init__(self, ip, operator, provider, created_at):
+        self.ip = ip
+        self.operator = operator
+        self.provider = provider
+        self.created_at = created_at
+        
+    def __repr__(self):
+        return self.toJson()
 
 if __name__ == '__main__':
     main()
